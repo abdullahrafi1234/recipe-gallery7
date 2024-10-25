e1ed32336bc8e71e2aa5fcef01c20a5ed4b464f9
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-10-25 19:57:59 +0600

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.css'
-import Header from './components/Header/Header'
+import './App.css';
+import Header from './components/Header/Header';
 import Main from "./components/Main/Main";
 
 function App() {
 
   return (
-    <div className='container mx-auto p-4 md:p-0'>
+    <div className='container mx-auto p-4 md:p-2'>
       
       <Header></Header>
       <Main></Main>
